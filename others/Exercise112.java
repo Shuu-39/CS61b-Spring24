28ef2ea0cc2e3de92b184e1a4715d917d3af1d7b
Shuu-39 <1397402891@qq.com>
2026-04-06 00:38:49 +0800

    new file:   others/Exercise112.class 	new file:   others/Exercise112.java 	modified:   others/HelloNumbers.class

---
+++ others/Exercise112.java
@@ -0,0 +1,14 @@
+package others;
+
+public class Exercise112 {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        int x = 1;
+        while (x < 11) {
+            System.out.print(sum + " ");
+            sum = sum + x;
+            x = x + 1;
+        }
+    }
+}
