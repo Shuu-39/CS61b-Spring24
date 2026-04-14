7090b7e67892a424e50c673e798fbd6d8f0104d3
tonchinn shuu <tonchinn-shuu@outlook.com>
2026-04-14 11:31:49 +0800

    完成61b环境配置

---
+++ others/ExerciseDogLauncher.java
@@ -0,0 +1,11 @@
+package others;
+
+import java.lang.foreign.SymbolLookup;
+
+public class ExerciseDogLauncher {
+    public static void main(String[] args) {
+        Dog.makeNoise();
+    }
+
+}
+
